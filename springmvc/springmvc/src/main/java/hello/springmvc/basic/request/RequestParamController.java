d36d96062b104ecf47ab680413c107225de9ccbf
Github ID <tlswlsdntks@naver.com>
2023-06-11 17:25:28 +0900

    * @RequestParam, @ModelAttribute

--- springmvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -46,6 +49,63 @@ public class RequestParamController {
         @RequestParam String username,
         @RequestParam int age){
         log.info("userName={}, age={}", username, age);
+        return "ok";
+    }
+
+    // localhost:8080/request-param-v4?username=1&age=2
+    @RequestMapping("/request-param-v4")
+    @ResponseBody
+    public String requestParamV4 (String username, int age){
+        log.info("userName={}, age={}", username, age);
+        return "ok";
+    }
+
+    // localhost:8080/request-param-required?username=id&age=2
+    // localhost:8080/request-param-required?username=id&age=
+    // age= -> Integer age는 null 가능이므로 에러가 발생하지않는다
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired (
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age){
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    // localhost:8080/request-param-default
+    // localhost:8080/request-param-default?username=&age=
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault (
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age){
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    // localhost:8080/request-param-map?username=id&age=1
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    // localhost:8080/request-attribute-v1?username=id&age=1
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    // localhost:8080/request-attribute-v2?username=id&age=1
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
 
