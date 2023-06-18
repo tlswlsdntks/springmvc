dd002760f14cbffa7cfc510d0b4379d0838563d8
tlswlsdntks <tlswlsdntks@naver.com>
2023-06-18 16:55:52 +0900

    * 기본 설정

--- item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -9,39 +9,5 @@ import java.util.Map;
 
 @Repository
 public class ItemRepository {
-
-    private static final Map<Long, Item> store = new HashMap<>();
-    private static long sequence = 0L;
-
-    public Item save(Item item){
-        item.setId(++sequence);
-        store.put(item.getId(), item);
-        return item;
-    }
-
-    public Item findById(Long id){
-
-        // id -> item
-        return store.get(id);
-    }
-
-    public List<Item> findAll(){
-
-        // items
-        return new ArrayList<>(store.values());
-    }
-
-    public void update(Long itemId, Item updateParam){
-
-        Item findItem = findById(itemId);
-        findItem.setItemName(updateParam.getItemName());
-        findItem.setPrice(updateParam.getPrice());
-        findItem.setQuantity(updateParam.getQuantity());
-    }
-
-    public void clearStore(){
-
-        // hashMap.claer()
-        store.clear();
-    }
+    
 }
