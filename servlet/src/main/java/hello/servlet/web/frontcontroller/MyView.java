5a7a10813dd1ffefe6e3fdac426f4ff7b4a40356
Github ID <tlswlsdntks@naver.com>
2023-06-27 23:20:02 +0900

    * frontcontroller map<uri, (interface)controller> -> return MyView

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,27 @@
+package hello.servlet.web.frontcontroller;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Getter @Setter
+public class MyView {
+
+    private String viewPath = "";
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
