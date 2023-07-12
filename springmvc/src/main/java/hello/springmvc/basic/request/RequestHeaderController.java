e50f60cbd02353d7c2d2a861005e6d6ccc4b27e7
Shin Jin-Woo <115134723+tlswlsdntks@users.noreply.github.com>
2023-07-12 18:34:14 +0900

    Update RequestHeaderController.java

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -22,7 +22,7 @@ public class RequestHeaderController {
         HttpServletResponse response,
         HttpMethod httpMethod,
         Locale locale,
-        @RequestHeader MultiValueMap<String, String> headerMap,
+        @RequestHeader MultiValueMap<String, Object> headerMap,
         @RequestHeader("host") String host,
         @CookieValue(value = "myCookie", required = false) String cookie){
         log.info("request={}", request);
