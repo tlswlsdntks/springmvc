a3d0b74bbf9f2442c4a946c345c09246682955f0
Github ID <tlswlsdntks@naver.com>
2023-07-13 23:07:24 +0900

    @RestController

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -8,12 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
-@Controller
+//@Controller
+@RestController
 public class ResponseBodyController {
     
     @GetMapping("/response-body-string-v1")
@@ -31,14 +33,14 @@ public class ResponseBodyController {
         return new ResponseEntity<>("ok", HttpStatus.OK);
     }
     
-    @ResponseBody
+//    @ResponseBody
     @GetMapping("/response-body-string-v3")
     public String responseBodyV3(){
         
         return "ok";
     }
     
-    @ResponseBody
+//    @ResponseBody
     @GetMapping("/response-body-json-v1")
     public ResponseEntity<HelloData> responseBodyJsonV1(){
         HelloData helloData = new HelloData();
@@ -47,15 +49,15 @@ public class ResponseBodyController {
         
         return new ResponseEntity<>(helloData, HttpStatus.OK);
     }
-    
+
+//    @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
     @GetMapping("/response-body-json-v2")
     public HelloData responseBodyJsonV2(){
         HelloData helloData = new HelloData();
         helloData.setUsername("userA");
         helloData.setAge(22);
-        
+
         return helloData;
     }
     
